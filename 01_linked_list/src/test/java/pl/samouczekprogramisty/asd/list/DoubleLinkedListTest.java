97b636ac077ae714ec9ce398d77ff7f54eb63d74
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-01-01 17:17:21 +0100

    Removing code duplication from tests.

--- 01_linked_list/src/test/java/pl/samouczekprogramisty/asd/list/DoubleLinkedListTest.java
+++ 01_linked_list/src/test/java/pl/samouczekprogramisty/asd/list/DoubleLinkedListTest.java
@@ -1,5 +1,6 @@
 package pl.samouczekprogramisty.asd.list;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
@@ -8,56 +9,58 @@ import static org.junit.Assert.fail;
 
 public class DoubleLinkedListTest {
 
+    private DoubleLinkedList<String> list;
+
+    @Before
+    public void setUp() {
+        list = new DoubleLinkedList<>();
+    }
+
     @Test
     public void shouldSayThatEmptyListIsEmpty() {
-        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         assertThat(list.isEmpty(), is(true));
     }
 
     @Test
     public void shouldSayThatListWithOneElementIsntEmpty() {
-        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
-        list.add(12);
+        list.add("12");
         assertThat(list.isEmpty(), is(false));
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void shouldThrowExceptionWhenFetchingFromNegativeIndex() {
-        new DoubleLinkedList().get(-1);
+        list.get(-1);
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void shouldThrowExceptionWhenFetchingFromEmptyList() {
-        new DoubleLinkedList().get(0);
+        list.get(0);
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void shouldTronwExceptionWhenRemovingFromEmptyList() {
-        new DoubleLinkedList().remove(0);
+        list.remove(0);
     }
 
     @Test
     public void shouldSayThatEmptyListHasSize0() {
-        assertThat(new DoubleLinkedList<Integer>().size(), is(0));
+        assertThat(list.size(), is(0));
     }
 
     @Test
     public void shouldGetValidSizeForListWithOneElement() {
-        DoubleLinkedList<String> list = new DoubleLinkedList<>();
         list.add("something)");
         assertThat(list.size(), is(1));
     }
 
     @Test
     public void shouldFetchElementFromList() {
-        SingleLinkedList<String> list = new SingleLinkedList<>();
         list.add("something");
         assertThat(list.get(0), is("something"));
     }
 
     @Test
     public void shouldGetSizeForListWithMoreElements() {
-        SingleLinkedList<String> list = new SingleLinkedList<>();
         list.add("something");
         list.add("else");
         list.add("is");
@@ -67,7 +70,6 @@ public class DoubleLinkedListTest {
 
     @Test
     public void shouldGetValidElementFromListWithMoreElements() {
-        SingleLinkedList<String> list = new SingleLinkedList<>();
         list.add("something");
         list.add("else");
         list.add("is");
@@ -80,7 +82,6 @@ public class DoubleLinkedListTest {
 
     @Test
     public void shouldBeAbleToClearList() {
-        SingleLinkedList<String> list = new SingleLinkedList<>();
         list.add("abc");
         list.clear();
 
@@ -89,7 +90,6 @@ public class DoubleLinkedListTest {
 
     @Test
     public void shouldReturnOldValueWhileSettingNew() {
-        SingleLinkedList<String> list = new SingleLinkedList<>();
         list.add("abc");
         String oldValue = list.set(0, "cab");
 
@@ -98,7 +98,6 @@ public class DoubleLinkedListTest {
 
     @Test
     public void shouldReplaceOldValueWithNew() {
-        SingleLinkedList<String> list = new SingleLinkedList<>();
         list.add("abc");
         list.set(0, "cab");
 
@@ -107,7 +106,6 @@ public class DoubleLinkedListTest {
 
     @Test
     public void shouldRemoveFirstElement() {
-        SingleLinkedList<String> list = new SingleLinkedList<>();
         list.add("abc");
         list.add("bc");
         list.add("c");
@@ -121,7 +119,6 @@ public class DoubleLinkedListTest {
 
     @Test
     public void shouldRemoveMiddleElement() {
-        SingleLinkedList<String> list = new SingleLinkedList<>();
         list.add("abc");
         list.add("bc");
         list.add("c");
@@ -135,7 +132,6 @@ public class DoubleLinkedListTest {
 
     @Test
     public void shouldRemoveLastElement() {
-        SingleLinkedList<String> list = new SingleLinkedList<>();
         list.add("abc");
         list.add("bc");
         list.add("c");
@@ -149,8 +145,6 @@ public class DoubleLinkedListTest {
 
     @Test
     public void shouldBeAbleToAddAtIndex() {
-        SingleLinkedList<String> list = new SingleLinkedList<>();
-
         list.add(0, "a");
 
         assertThat(list.size(), is(1));
@@ -159,8 +153,6 @@ public class DoubleLinkedListTest {
 
     @Test
     public void shouldKeepAddingAtIndex0() {
-        SingleLinkedList<String> list = new SingleLinkedList<>();
-
         list.add(0, "a");
         list.add(0, "b");
         list.add(0, "c");
@@ -173,13 +165,11 @@ public class DoubleLinkedListTest {
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void shouldThrowAnExceptionWhenAddingOnNonExistingIndex() {
-        SingleLinkedList<String> list = new SingleLinkedList<>();
         list.add(1, "a");
     }
 
     @Test
     public void shouldThrowAnExceptionWhenAddingOnNonExistingIndexWithAlreadyFilledList() {
-        SingleLinkedList<String> list = new SingleLinkedList<>();
         list.add(0, "a");
 
         try {
@@ -192,8 +182,6 @@ public class DoubleLinkedListTest {
 
     @Test
     public void shouldKeepAddingAtTheEnd() {
-        SingleLinkedList<String> list = new SingleLinkedList<>();
-
         list.add(0, "a");
         list.add(1, "b");
         list.add(2, "c");
